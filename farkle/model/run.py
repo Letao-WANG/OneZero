7d1133a04e812f5ae90f2dec7b8e1538692af8f7
Letao <letaowang7@gmail.com>
2022-01-21 12:48:42 +0100

    Syu

--- farkle/model/run.py
+++ farkle/model/run.py
@@ -2,7 +2,8 @@ from state import *
 
 state_date = StateDate(roll_dice(6), '')
 state = State(state_date, 0, 0, need_to_score=True)
+print("state: " + str(state))
+print("combos: " + str(state.available_combos))
+print("next: " + str(state.next_states))
 
-print(state)
-state = state.next_states
-print(state)
+
